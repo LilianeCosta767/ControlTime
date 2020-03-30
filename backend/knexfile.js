1c294317f7d082622d823401a073364de647619c
LilianeCosta767 <lilianecosta767@hotmail.com>
2020-03-30 10:44:37 -0300

    configuracao do banco sqlite3 e criacao das rotas da aplicacao

---
+++ backend/knexfile.js
@@ -0,0 +1,44 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: { //desenvolvimento
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
+    }
+  },
+
+  staging: { //simulando a produção para testes
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: { //produção
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
